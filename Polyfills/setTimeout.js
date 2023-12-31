b4ea8eeb6cf8cc134f0ad7c047aff4db482f628f
abhishek9125 <abhishek4075@gmail.com>
2023-12-31 21:45:48 +0530

    Polyfill: setTimeout

---
+++ Polyfills/setTimeout.js
@@ -0,0 +1,42 @@
+function createSetTimeOut() {
+
+    let timerId = 0;
+    let timerMap = {};
+    
+    function mySetTimeout(callback, delay, ...args) {
+        let id = timerId++;
+        timerMap[id] = true;
+
+        const start = Date.now();
+
+        function triggerCallback() {
+            if(!timerMap[id]) return;
+            if(Date.now() > start + delay) {
+                callback.apply(this, args);
+            } else {
+                requestIdleCallback(triggerCallback);
+            }
+        }
+        requestIdleCallback(triggerCallback);
+        return id;
+    }
+
+    function myClearInterval(id) {
+        delete timerMap[id];
+    }
+
+    return { mySetTimeout, myClearInterval }
+}
+
+var { mySetTimeout, myClearInterval } = createSetTimeOut();
+
+console.log('Start');
+
+const myTimeout = mySetTimeout(function(name) {
+    console.log('This is Set Timeout Polyfill');
+    console.log('My Name is ', name);
+}, 1, "Abhishek")
+
+myClearInterval(myTimeout)
+
+console.log('End');
